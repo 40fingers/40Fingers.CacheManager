f5ef41dbec68e2bf0b6c84fb9e5fead80c821a58
Stefan Kamphuis <stefan@superska.net>
2020-07-10 14:39:09 +0200

    Add project files.

---
+++ Components/BaseClasses/ApiControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetNuke.Web.Api;
+
+namespace FortyFingers.CacheManager.Components.BaseClasses
+{
+    public class ApiControllerBase : DnnApiController
+    {
+        private const string DBCONTEXT_KEY = "CacheManagerContext_Instance";
+
+    }
+}
---
+++ Services/CacheManagerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using DotNetNuke.Common;
+using DotNetNuke.Web.Api;
+using DotNetNuke.Security;
+using System.Threading;
+using DotNetNuke.UI.Modules;
+using DotNetNuke.Common.Utilities;
+using System.Collections.Generic;
+using FortyFingers.CacheManager.Components.BaseClasses;
+using DotNetNuke.Web.Client.ClientResourceManagement;
+using Newtonsoft.Json.Linq;
+
+namespace FortyFingers.CacheManager.Services
+{
+    [SupportedModules("40F CacheManager")]
+    [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.View)]
+    public class CacheManagerController : ApiControllerBase
+    {
+        public CacheManagerController() { }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public HttpResponseMessage ClearCache()
+        {
+            DataCache.ClearCache();
+            ClientResourceManager.ClearCache();
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.NoContent);
+        }
+    }
+}
